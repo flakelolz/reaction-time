be07f75a4fadfc7d8ec75ad34d199194b41dc8d3
Michael Gonzalez <michaelgonzalez.code@gmail.com>
2024-02-28 11:37:11 -0400

    Added coutdown UI

--- src/countdown.rs
+++ src/countdown.rs
@@ -1,11 +1,21 @@
 use bevy::prelude::*;
 
+use crate::reaction::ReactionState;
+
 pub struct CountdownPlugin;
 
 impl Plugin for CountdownPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(TimeKeeper::new(3.0))
-            .add_systems(Update, countdown_ui_system);
+            .add_systems(Startup, setup_countdown_ui)
+            .add_systems(
+                Update,
+                show_countdown.run_if(in_state(ReactionState::Countdown)),
+            )
+            .add_systems(
+                Update,
+                hide_countdown.run_if(not(in_state(ReactionState::Countdown))),
+            );
     }
 }
 
@@ -22,9 +32,67 @@ impl TimeKeeper {
     }
 }
 
-fn countdown_ui_system(mut timer: ResMut<TimeKeeper>, time: Res<Time>) {
-    if !timer.countdown.tick(time.delta()).just_finished() {
-        let count = timer.countdown.remaining_secs();
-        // println!("{:.2?}", count);
+#[derive(Component)]
+struct CountdownUi;
+
+fn setup_countdown_ui(mut commands: Commands, timer: Res<TimeKeeper>) {
+    let container = NodeBundle {
+        style: Style {
+            position_type: PositionType::Absolute,
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            width: Val::Percent(100.),
+            height: Val::Percent(100.),
+            ..default()
+        },
+        ..default()
+    };
+
+    let count = (
+        TextBundle::from_section(
+            format!("{:.2}", timer.countdown.remaining_secs()),
+            TextStyle {
+                // font: asset_server.load("fonts/04B_19__.ttf"),
+                font_size: 150.,
+                color: Color::WHITE,
+                ..default()
+            },
+        )
+        .with_style(Style {
+            display: Display::None,
+            position_type: PositionType::Absolute,
+            ..default()
+        }),
+        CountdownUi,
+    );
+
+    let parent = commands.spawn(container).id();
+    let child = commands.spawn(count).id();
+    commands.entity(parent).push_children(&[child]);
+}
+
+// Shows and ticks the countdown while on ReactionState::Countdown state
+fn show_countdown(
+    mut text_query: Query<(&mut Text, &mut Style), With<CountdownUi>>,
+    mut timer: ResMut<TimeKeeper>,
+    time: Res<Time>,
+) {
+    timer.countdown.tick(time.delta());
+
+    for (mut text, mut style) in text_query.iter_mut() {
+        style.display = Display::default();
+        text.sections[0].value = format!("{:.2}", timer.countdown.remaining_secs());
+    }
+}
+
+// Hides and resets the countdown on any other state
+fn hide_countdown(
+    mut text_query: Query<&mut Style, With<CountdownUi>>,
+    mut timer: ResMut<TimeKeeper>,
+) {
+    timer.countdown.reset();
+
+    for mut style in text_query.iter_mut() {
+        style.display = Display::None;
     }
 }
